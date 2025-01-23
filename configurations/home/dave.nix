5c0b9d2f3052b769468b8e27e359d52055707b75
Dave English <4254983+lutnos@users.noreply.github.com>
2025-01-23 10:22:34 +0000

    add sdkman

--- configurations/home/dave.nix
+++ configurations/home/dave.nix
@@ -20,4 +20,9 @@ in
   home.username = "dave";
   home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/dave";
   home.stateVersion = "24.11";
+
+  programs.zsh.initExtra = ''
+    export SDKMAN_DIR="/Users/dave/.sdkman"
+    [[ -s "/Users/dave/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/dave/.sdkman/bin/sdkman-init.sh"
+    '';
 }
