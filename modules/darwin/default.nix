ade0202569d3b21396a7a52438ed8a35f9a1ea46
Dave English <4254983+lutnos@users.noreply.github.com>
2026-02-10 11:23:36 +0000

    Revert "Add vscode"
    This reverts commit 94180b3fb4531a1f940c78ba255d4b0af24e6afc.

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,13 +1,8 @@
 # This is your nix-darwin configuration.
 # For home configuration, see /modules/home/*
-{ lib, ... }:
 {
   imports = [
     ./common
-  ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "vscode"
   ];
 
   # Use TouchID for `sudo` authentication
